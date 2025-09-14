4fef6c19aa7a40e037d36e96f87341bee7fc2caf
Aaron Lam <aaronlam1004@gmail.com>
2025-09-13 23:15:11 -0700

    Cleaning up description in About section

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -1,29 +1,14 @@
 <script setup lang="ts">
 import Section from '@/components/Section.vue';
-import profileImg from '@/assets/profile.jpg';
 </script>
 
 <template>
-  <Section :image-src="profileImg">
+  <Section>
     <template #title>About</template>
     <div class="content">
       <strong>Hello!</strong>
       <p>
-        I am a software engineer doing full-stack and embedded development.
-      </p>
-      <p>
-        Some of my experience includes:
-      </p>
-      <ul>
-        <li>Mobile Apps</li>
-        <li>Web Apps</li>
-        <li>Server Services</li>
-        <li>Touch Applications</li>
-        <li>Control Systems</li>
-        <li>Embedded Drivers</li>
-      </ul>
-      <p>
-        On my free time I like playing games with friend and am currently trying to learn new things.
+        I am a software engineer with embedded, web, and mobile development experience.
       </p>
     </div>
   </Section>
